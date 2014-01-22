506ae83a48c35f1cc50ccf9c4ae5b8376cef0ca4
Sundar Visweswaran <vsundar723@gmail.com>
2014-01-21 19:46:45 -0600

    added message, js

--- app/assets/javascripts/cases_controller.js
+++ app/assets/javascripts/cases_controller.js
@@ -1,13 +1,56 @@
+$(document).ready(function() {
+  $('#create-label')[0].reset();
+  $('#form-case').prop('disabled', true);
+  $('#submit-and-apply').prop('disabled', true);
+});
+
+
+$(document).on('click', '#form-enabled', function() {
+  if ($('#form-enabled').prop('checked')) {
+    $('#form-case').prop('disabled', false);
+    $('#submit-and-apply').prop('disabled', false);
+  } else {
+    $('#form-case').prop('disabled', true);
+    $('#submit-and-apply').prop('disabled', true);
+  }
+});
+
+function sendData($form, data, url) {
+  $.ajax({
+    url: url,
+    type: $form.attr('method'),
+    data: data,
+    contentType: 'application/json',
+    dataType: 'json',
+    async: false,
+    success: function(data) {
+      if(data && ((data.status === '201 Created') || (data.status === '200 OK'))) {
+        $('#alert').empty().append(
+          '<div class="alert alert-success alert-dismissable">' +
+            '<button type="button" class="close" data-dismiss="alert">' +
+            '&times;</button>' + 'Successfully Saved.' +
+            '<a href="' + window.location.origin + '" class="alert-link">Refresh</a> to see the changes.' +
+          '</div>');
+      } else {
+        $('#alert').empty().append(
+          '<div class="alert alert-danger alert-dismissable">' +
+            '<button type="button" class="close" data-dismiss="alert">' +
+            '&times;</button>' + 'Something went wrong' + '</div>');
+      }
+    },
+  });
+}
+
 $(document).on('click', '#submit-and-apply', function(e){
   var $form = $('#create-label');
   var $data = {
     name: $('#formName').val(),
     description: $('#form-desc').val(),
     types: $('#form-types').val(),
-    enabled: $('#form-enabled').prop("checked"),
+    enabled: $('#form-enabled').prop('checked'),
     color: $('#form-color').val(),
     selected_case: $('#form-case').val()
-  }
+  };
   e.preventDefault();
   sendData($form, $data, '/cases/createAndApply');
 });
@@ -18,22 +61,9 @@ $(document).on('click', '#submit', function(e){
     name: $('#formName').val(),
     description: $('#form-desc').val(),
     types: $('#form-types').val(),
-    enabled: $('#form-enabled').prop("checked"),
+    enabled: $('#form-enabled').prop('checked'),
     color: $('#form-color').val()
-  }
+  };
   e.preventDefault();
   sendData($form, $data, '/cases/create');
 });
-
-function sendData($form, data, url) {
-  $.ajax({
-    url: url,
-    type: $form.attr('method'),
-    data: data,
-    contentType: 'application/json',
-    dataType: 'json',
-    success: function(data){
-      alert('successfully submitted')
-    }
-  });
-}
--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -21,16 +21,18 @@ class CasesController < ApplicationController
 
   # Create label action called when create button in the form is clicked
   def create_label
-    json = create_json
-    response = client.create_label(@access_token, json)
+    response = client.create_label(@access_token, create_json)
+    render :json => response, :status => :ok
   end
 
   # Create label and apply action called when create and apply button in the form is clicked.
   # This takes the id of the case to apply the label from the params.
   def create_label_and_apply
-    label_json = create_label
-    case_id = params[:selected_case]
-    response = client.apply_label_to_case(@access_token, case_id, JSON.parse(label_json.body))
+    label_json = client.create_label(@access_token, create_json)
+    if label_json.code == '201'
+      response = client.apply_label_to_case(@access_token, params[:selected_case], JSON.parse(label_json.body))
+    end
+    render :json => response, :status => :ok
   end
 
   # Creates JSON from the params.
